f5b32a2f75a1add6ae0a1e900fb2f25b6e89182f
Jazmin Matos <jmatos1993@gmail.com>
2020-08-21 23:37:02 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,29 @@
+#1
 def using_include(array, element)
 
 end
 
+#2
 def using_sort(array)
 
 end
 
+#3
 def using_reverse(array)
 
 end
 
+#4
 def using_first(array)
 
 end
 
+#5
 def using_last(array)
 
 end
 
+#6
 def using_size(array)
 
 end
